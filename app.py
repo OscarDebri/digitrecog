0263ccda6e576fd5e42f851c15457970ec05c11f
OscarDebri <143581576+OscarDebri@users.noreply.github.com>
2026-07-15 07:50:13 -0600

    trivis

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Input, Flatten, Dense
+import threading
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import tensorflow as tf
+from PIL import Image, ImageOps
+import io
+import base64
+import re
+
+import threading
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import tensorflow as tf
+from PIL import Image, ImageOps
+import io
+import base64
+import re
